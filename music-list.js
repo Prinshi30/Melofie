27c2c8cd55d8a4dfa68cb7e809e8b619f9a7f7a5
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-07-26 21:46:51 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,98 @@
+let allMusic = [
+    {
+        name: "Aadat Se Majboor",
+        artist: "Benny Dayal",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Pyaar Hota Kayi Baar Hai",
+        artist: "Arijit Singh",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Bhool Bhulaiyaa 2",
+        artist: "Neeraj Shridhar",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Hale Dil",
+        artist: "Harshit Saxena",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Dil Sambhal Ja Zara",
+        artist: "Mohd. Irfan, Arijit Singh and Salim Bhatt. ",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Shape-of-You",
+        artist: "Ed Sheeran",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Calm-Down",
+        artist: "Rema, Selena Gomez",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Let Me Down Slowly",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Love Me Like You Do",
+        artist: "Ellie Goulding",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Girls Like You",
+        artist: "Maroon 5",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Sorry",
+        artist: "Justin Beiber",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sugar and Brownies",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Woh",
+        artist: "Dino, Ikka & Badshah ",
+        img: "music-16",
+        src: "music-16"
+    },
+];
